03d5282cfdc027351d049e4fe26cd52e4dbbb2a6
suwanshree <suwanshree@gmail.com>
2023-03-03 08:53:37 -0800

    Update: Frontend dependencies added

---
+++ frontend_challenge/src/pages/Home.jsx
@@ -0,0 +1,39 @@
+// New Landing Page
+import { Text, Stack, Center, Image, Button, Box } from "@chakra-ui/react";
+import Mug from "../assets/svgs/mug.svg";
+
+function Home() {
+  const lorem =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus gravida enim purus, eget commodo metus. Phasellus gravida enim purus. ";
+  return (
+    <Center display="flex" justifyContent="center" w="100vw" h="100vh">
+      <Stack align="center" spacing="5">
+        <Image
+          objectFit="cover"
+          src={Mug}
+          alt="Mug Icon"
+          w={["20vw", "10vw"]}
+        />
+        <Text
+          fontSize="6xl"
+          fontWeight="bold"
+          w={["90vw", "30vw"]}
+          textAlign="center"
+        >
+          Coffee Blog Home
+        </Text>
+        <Stack spacing="2">
+          <Text w={["90vw", "30vw"]} textAlign="center">
+            {lorem}
+          </Text>
+          <Text w={["90vw", "30vw"]} textAlign="center">
+            {lorem}
+          </Text>
+        </Stack>
+        <Button>Continue</Button>
+      </Stack>
+    </Center>
+  );
+}
+
+export default Home;
