0a694bfba715da38cc87b61271fb5c9da67d4cca
suwanshree <suwanshree@gmail.com>
2023-03-04 18:23:38 -0800

    Feature: Custom modal modified to handle different usecases

--- frontend_challenge/src/components/CustomModal.jsx
+++ frontend_challenge/src/components/CustomModal.jsx
@@ -11,6 +11,9 @@ import {
   FormControl,
   FormLabel,
   Input,
+  Divider,
+  Select,
+  Textarea,
 } from "@chakra-ui/react";
 
 export const CustomModal = ({ onClose, isOpen, onSubmit, variant }) => {
@@ -26,6 +29,12 @@ export const CustomModal = ({ onClose, isOpen, onSubmit, variant }) => {
     onSubmit();
   };
 
+  const handlePostSubmit = (event) => {
+    event.preventDefault();
+    onClose();
+    onSubmit();
+  };
+
   const newCoffeeModal = (
     <Modal
       onClose={onClose}
@@ -36,7 +45,7 @@ export const CustomModal = ({ onClose, isOpen, onSubmit, variant }) => {
       <ModalOverlay backdropFilter="blur(10px)" zIndex="1" />
       <ModalContent
         flexDir="column"
-        padding="5"
+        padding="2"
         justifyContent="center"
         alignItems="center"
         borderRadius="20"
@@ -45,19 +54,33 @@ export const CustomModal = ({ onClose, isOpen, onSubmit, variant }) => {
       >
         <ModalHeader fontSize="3xl">New Coffee</ModalHeader>
         <ModalCloseButton />
+        <Divider
+          borderColor={colorMode === "light" ? "brown" : "cyan"}
+          size="100"
+        />
         <form onSubmit={handleCoffeeSubmit}>
           <ModalBody>
-            <FormControl>
-              <FormLabel>Name</FormLabel>
-              <Input type="text" name="name" />
+            <FormControl display="flex" alignItems="center" marginTop="20px">
+              <FormLabel>Name:</FormLabel>
+              <Input type="text" name="name" width="250px" marginLeft="40px" />
             </FormControl>
-            <FormControl>
-              <FormLabel>Year</FormLabel>
-              <Input type="number" name="year" />
+            <FormControl display="flex" alignItems="center" marginTop="20px">
+              <FormLabel>Year:</FormLabel>
+              <Input
+                type="number"
+                name="year"
+                width="250px"
+                marginLeft="54px"
+              />
             </FormControl>
-            <FormControl>
-              <FormLabel>Caffeine</FormLabel>
-              <Input type="number" name="caffeine" />
+            <FormControl display="flex" alignItems="center" marginTop="20px">
+              <FormLabel>Caffine:</FormLabel>
+              <Input
+                type="number"
+                name="caffeine"
+                width="250px"
+                marginLeft="35px"
+              />
             </FormControl>
           </ModalBody>
           <ModalFooter
@@ -91,6 +114,54 @@ export const CustomModal = ({ onClose, isOpen, onSubmit, variant }) => {
       >
         <ModalHeader fontSize="3xl">New Post</ModalHeader>
         <ModalCloseButton />
+        <Divider
+          borderColor={colorMode === "light" ? "brown" : "cyan"}
+          size="100"
+        />
+
+        <form onSubmit={handlePostSubmit}>
+          <ModalBody>
+            <FormControl marginTop="20px">
+              <Input
+                type="text"
+                name="title"
+                width="100%"
+                placeholder="Title"
+              />
+            </FormControl>
+            <FormControl marginTop="20px">
+              <Input
+                type="number"
+                name="rating"
+                width="100%"
+                placeholder="Rating"
+              />
+            </FormControl>
+            <FormControl display="flex" alignItems="center" marginTop="20px">
+              <FormLabel>Coffee:</FormLabel>
+              <Select name="coffee" width="200px" marginLeft="34px">
+                <option value="columbian">Columbian</option>
+                <option value="ethiopian">Ethiopian</option>
+                <option value="guatemalan">Guatemalan</option>
+              </Select>
+            </FormControl>
+            <FormControl>
+              <Textarea
+                name="text"
+                width="100%"
+                placeholder="Post Text"
+                marginTop="20px"
+              />
+            </FormControl>
+          </ModalBody>
+          <ModalFooter
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Button type="submit">Submit</Button>
+          </ModalFooter>
+        </form>
       </ModalContent>
     </Modal>
   );
